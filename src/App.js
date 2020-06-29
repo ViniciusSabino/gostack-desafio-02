ffe89a780fd85bacd72042a5cb3f83d371806063
Vinicius Rocha <vnc.chelsea@gmail.com>
2020-06-28 21:20:53 -0300

    feat: finalizing react concepts challenge

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
-function App() {
-  async function handleAddRepository() {
-    // TODO
+const App = () => {
+  const [ repositories, setRepositories ] = useState([]);
+  const [ repository, setRepository ] = useState('');
+
+  // Hooks
+  useEffect(() => {
+    const getRepositories = async () => {
+      const { data } = await api.get('/repositories');
+      setRepositories(data);
+    }
+    
+    getRepositories();
+  }, [])
+
+  // Actions
+  const handleChangeAddRepository = (e) => {
+    setRepository(e.target.value);
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+  const handleAddRepository = async () => {
+    const { data } = await api.post('/repositories', {
+      title: repository
+    })
+
+    setRepositories([...repositories, data]);
+    setRepository('');
+  }
+
+  const handleRemoveRepository = async (id) => {
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
+        {repositories.map((repository) => 
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li> 
+        )}
+      </ul> 
+      <input type="text" name="title" minLength="2" maxLength="15" onChange={handleChangeAddRepository} value={repository}/>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
